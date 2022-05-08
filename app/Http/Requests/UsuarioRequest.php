d404edbd73e2488c26bdeccd2debe9764c6d30fa
Jesus Villegas <jesus777.jsgl@gmail.com>
2022-05-08 14:31:45 -0500

    Request usuario para validacion

---
+++ app/Http/Requests/UsuarioRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UsuarioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'nombre'   => 'required|string|min:3|max:50',
+            'apellido' => 'required|string|min:3|max:100',
+            'edad'     => 'required|integer|between:0,127',
+        ];
+    }
+}
