23e8662255d80ee1df61462952d1f26fe22057ad
Jesus Villegas <jesus777.jsgl@gmail.com>
2022-05-08 14:13:17 -0500

    Vue agregado al proyecto

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+require("./bootstrap");
+
+// Agregamos vue3
+import { createApp } from "vue";
+
+const app = createApp({});
+
+app.component(
+    "pagina-inicio",
+    require("./components/PaginaInicio.vue").default
+);
+
+app.mount("#app");
---
+++ resources/js/components/PaginaInicio.vue
@@ -0,0 +1,7 @@
+<template>Desde el componente PaginaInicio</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
